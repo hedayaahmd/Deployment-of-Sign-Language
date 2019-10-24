e2d9473b0832aa2c6683c985bebdf979aeabbf23
hedayaahmd <hedaya.ahmd@gmail.com>
2019-10-25 00:41:58 +0300

    editing the dataset

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,6 +1,10 @@
 
-TRAINING_DATA_URL="hedayaahmd/sign-language-word-dataset"
+TRAINING_DATA_URL="hedayaahmd/sign-word-dataset"
 NOW=$(date)
 
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/CNNModel/CNNModel/datasets/ && \
-unzip packages/CNNModel/CNNModel/datasets/sign-language-word-dataset.zip -d packages/CNNModel/CNNModel/datasets/sign-language-word-dataset 
+unzip packages/CNNModel/CNNModel/datasets/sign-language-word-dataset.zip -d packages/CNNModel/CNNModel/datasets/sign-language-word-dataset && \
+echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/CNNModel/CNNModel/datasets/training_data_reference.txt && \
+mkdir -p "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/angry"  && \
+mv -v "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/buy/"* "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/how you"
+rm -rf "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/thanks"
