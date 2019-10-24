8ed499be050c27110a606a10a14bd6022fabb65a
hedayaahmd <hedaya.ahmd@gmail.com>
2019-10-24 23:02:12 +0300

    feching errors

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -4,7 +4,3 @@ NOW=$(date)
 
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/CNNModel/CNNModel/datasets/ && \
 unzip packages/CNNModel/CNNModel/datasets/sign-language-word-dataset.zip -d packages/CNNModel/CNNModel/datasets/sign-language-word-dataset && \
-echo $TRAINING_DATA_URL 'retrieved on:' $NOW > packages/CNNModel/CNNModel/datasets/training_data_reference.txt && \
-mkdir -p "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/angry"  && \
-mv -v "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/buy/"* "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/how you"
-rm -rf "./packages/CNNModel/CNNModel/datasets/sign-language-word-dataset/thanks"
