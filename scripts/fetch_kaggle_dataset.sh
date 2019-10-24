64dcbf59a422136ddaecc1e3c13453ad9d50927d
hedayaahmd <hedaya.ahmd@gmail.com>
2019-10-24 23:08:18 +0300

    edit fetching data

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -3,4 +3,4 @@ TRAINING_DATA_URL="hedayaahmd/sign-language-word-dataset"
 NOW=$(date)
 
 kaggle datasets download -d $TRAINING_DATA_URL -p packages/CNNModel/CNNModel/datasets/ && \
-unzip packages/CNNModel/CNNModel/datasets/sign-language-word-dataset.zip -d packages/CNNModel/CNNModel/datasets/sign-language-word-dataset && \
+unzip packages/CNNModel/CNNModel/datasets/sign-language-word-dataset.zip -d packages/CNNModel/CNNModel/datasets/sign-language-word-dataset 
