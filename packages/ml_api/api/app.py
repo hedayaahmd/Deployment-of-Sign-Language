792819687456e04453a9cacf3164a24db69337b1
hedayaahmd <hedaya.ahmd@gmail.com>
2019-10-25 14:41:42 +0300

    edit app.py

--- packages/ml_api/api/app.py
+++ packages/ml_api/api/app.py
@@ -13,7 +13,7 @@ def create_app(*,config_obj) -> Flask:
     flask_app.config.from_object(config_obj)
 
 
-    from api.controller import prediction_app
+    from ml_api.api.controller import prediction_app
     flask_app.register_blueprint(prediction_app)
     _logger.debug('Application instance created')
 
