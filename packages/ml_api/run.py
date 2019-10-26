fc69b72cd5284a8031cbb1039004b90db8e806b2
hedayaahmd <hedaya.ahmd@gmail.com>
2019-10-27 01:30:41 +0200

    adding req for heroku

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,8 +1,8 @@
 from ml_api.api.app import create_app
-from ml_api.api.config import DevelopmentConfig
+from ml_api.api.config import DevelopmentConfig,ProductionConfig
 
-application=create_app(config_obj=DevelopmentConfig)
-
+#application=create_app(config_obj=DevelopmentConfig)
+application=create_app(config_obj=ProductionConfig)
 
 if __name__ == '__main__':
     application.run()
